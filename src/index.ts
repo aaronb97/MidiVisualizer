f27ab7e0fb5b038b11256acd98c27b11097d8c18
aaronb97 <aaron9becker7@gmail.com>
2021-11-05 16:42:48 -0400

    basic shape drawing capability

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import WebMidi from "webmidi";
+import * as THREE from "three";
+
+let camera, scene: THREE.Scene, renderer;
+let geometry, material, mesh;
 
 WebMidi.enable(function (err) {
   if (err) {
@@ -10,6 +14,54 @@ WebMidi.enable(function (err) {
       console.log(
         "Received 'noteon' message (" + e.note.name + e.note.octave + ")."
       );
+      addShape();
     });
   }
 });
+
+init();
+
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 1;
+
+  scene = new THREE.Scene();
+
+  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  material = new THREE.MeshNormalMaterial();
+
+  mesh = new THREE.Mesh(geometry, material);
+  //   scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animation);
+  document.body.appendChild(renderer.domElement);
+}
+
+function animation(time) {
+  mesh.rotation.x = time / 2000;
+  mesh.rotation.y = time / 1000;
+
+  renderer.render(scene, camera);
+}
+
+function addShape() {
+  const ball = new THREE.SphereGeometry(0.02);
+  const material = new THREE.MeshBasicMaterial();
+  material.color = new THREE.Color(Math.random(), Math.random(), Math.random());
+  const mesh = new THREE.Mesh(ball, material);
+  console.log(mesh.position);
+  mesh.position.set(
+    Math.random() * 2 - 1,
+    Math.random() * 2 - 1,
+    Math.random() * 2 - 1
+  );
+
+  scene.add(mesh);
+}
