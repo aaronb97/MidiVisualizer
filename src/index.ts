e933664ae8b5d18df7fb4de67bc14344804f3cab
aaronb97 <aaron9becker7@gmail.com>
2021-11-05 14:25:32 -0400

    process midi note inputs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-const test: string[] = ["test 5"];
-console.log(test);
+import WebMidi from "webmidi";
+
+WebMidi.enable(function (err) {
+  if (err) {
+    console.log("WebMidi could not be enabled.", err);
+  } else {
+    console.log("WebMidi enabled!");
+    const input = WebMidi.inputs[0];
+    input.addListener("noteon", "all", function (e) {
+      console.log(
+        "Received 'noteon' message (" + e.note.name + e.note.octave + ")."
+      );
+    });
+  }
+});
